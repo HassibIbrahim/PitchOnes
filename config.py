de556b31b4c3a43dd9d458b78a6af7447f747334
Hassib Ibrahim <ihassib@gmail.comclearcd cleargit -lgit --lostgit config --global user.email ihassib@gmail.com>
2018-11-18 19:56:39 +0300

    dealt eith the config classes

--- config.py
+++ config.py
@@ -21,11 +21,28 @@ class TestConfig(Config):
 
 
 class ProdConfig(Config):
-    """ 
+    """
     Production configuration child class
 
     Args:
         Config: The parent configuration class with General
         configuration settings
     """
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL") 
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+
+
+class DevConfig(Config):
+    """
+    Development configuration child class
+    Args:
+    Config: The parent configuration class with General
+    configuration settings
+    """
+SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://shabbaman:Kwanini@localhost/pitch'
+DEBUG = True
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig,
+'test':TestConfig
+} 
