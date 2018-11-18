948d9582bba4e8637f4ea30716171792af581175
Hassib Ibrahim <ihassib@gmail.comclearcd cleargit -lgit --lostgit config --global user.email ihassib@gmail.com>
2018-11-18 22:41:06 +0300

    added comments and category route

--- app/main/views.py
+++ app/main/views.py
@@ -6,9 +6,23 @@ from .. import db, photos
 from ..models import User, Post, Comment
 
 
-
 @main.route('/')
 def index():
-    title = 'Home - Welcome to Pitch Me This'
-    
+    title = 'Home - Welcome to Pitch Ones'
+
     return render_template('index.html', title=title)
+
+
+@main.route('/category/<ct_name>')
+def category(ct_name):
+    category = ct_name
+    title = f'{category}'
+    posts = Post.get_posts(category)
+    return render_template('category.html', title=title, category=category, posts=posts)
+
+
+@main.route('/category/comments/<int:id>')
+def comments(id):
+    title = 'Comments'
+    comments = Comment.get_comments(id)
+    return render_template('comments.html', title=title, comments=comments)
