7e88a00655d723ba47d5165376d00c9e69af06c6
Hassib Ibrahim <ihassib@gmail.comclearcd cleargit -lgit --lostgit config --global user.email ihassib@gmail.com>
2018-11-20 16:28:08 +0300

    added extensions usable to requirements

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -9,10 +9,8 @@ from ..models import User
 class RegistrationForm(FlaskForm):
     email = StringField('Your Email Address', validators=[Required(), Email()])
     username = StringField('Enter your username', validators=[Required()])
-    password = PasswordField('Password', validators=[Required(), EqualTo(
-        'password_confirm', message='Passwords must match')])
-    password_confirm = PasswordField(
-        'Confirm Passwords', validators=[Required()])
+    password = PasswordField('Password', validators=[Required(), EqualTo('password_confirm', message='Passwords must match')])
+    password_confirm = PasswordField('Confirm Password', validators=[Required()])
     submit = SubmitField('Sign Up')
 
     def validate_email(self, data_field):
