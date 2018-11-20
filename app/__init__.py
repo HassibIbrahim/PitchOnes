7fd480f9c8242b56dd10027068db64f4df7c23c8
Hassib Ibrahim <ihassib@gmail.comclearcd cleargit -lgit --lostgit config --global user.email ihassib@gmail.com>
2018-11-20 16:20:40 +0300

    app modification and styling the static folder

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,6 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_uploads import UploadSet, configure_uploads, IMAGES
 from flask_mail import Mail
-from flask_simplemde import SimpleMDE
 from flask_bootstrap import Bootstrap
 
 bootstrap = Bootstrap()
@@ -13,7 +12,7 @@ login_manager = LoginManager()
 login_manager.session_protection = 'strong'
 login_manager.login_view = 'auth.login'
 mail = Mail()
-simple = SimpleMDE()
+
 photos = UploadSet('photos', IMAGES)
 
 
@@ -30,7 +29,6 @@ def create_app(config_name):
     db.init_app(app)
     login_manager.init_app(app)
     mail.init_app(app)
-    simple.init_app(app)
     bootstrap.init_app(app)
 
 
